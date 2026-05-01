b60aa5a63c6be46d06acefc2c6e8cc7316226327
11259a565-hash <11259a565@kanchiuniv.ac.in>
2026-05-01 12:06:39 +0530

    Insertion Sort

---
+++ program_23.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int n, i, j, key, a[100];
+    printf("Enter size: ");
+    scanf("%d", &n);
+    for (i = 0; i < n; i++) scanf("%d", &a[i]);
+    for (i = 1; i < n; i++) {
+        key = a[i]; j = i - 1;
+        while (j >= 0 && a[j] > key) {
+            a[j + 1] = a[j];
+            j = j - 1;
+        }
+        a[j + 1] = key;
+    }
+    for (i = 0; i < n; i++) printf("%d ", a[i]);
+    return 0;
+}
