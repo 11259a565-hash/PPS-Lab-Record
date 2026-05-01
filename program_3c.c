d1584b43080cb95fab6a53d2d286b7f822f30fe4
11259a565-hash <11259a565@kanchiuniv.ac.in>
2026-05-02 00:00:17 +0530

    sqrt eq
    Fix syntax errors in printf and scanf statements.

---
+++ program_3c.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+  float a,b,c,s;
+  printf(Enter the values of a,b,c,s");
+  scanf("%f,%f,%f,%f",&a,&b,&c,&s);
+  printf("The equation is %.2f", sqrt(s*(s-a)*(s-b)*(s-c));
+return 0;
+}
