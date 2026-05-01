35e18a4036fec360eb1f74df96f49e3c9114654f
11259a565-hash <11259a565@kanchiuniv.ac.in>
2026-05-01 11:48:03 +0530

    Create program_6.c
    Fix input format specifiers and output format.

---
+++ program_6.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main() 
+{
+  float a,b;
+  printf("Enter the value of a");
+  scanf("%d", &a);
+  printf("Enter the value of b");
+  scanf("%d", &b);
+  if(a>b)
+    printf("%d is greater than %d", a,b);
+else
+    printf("%d is greater than %d", b,a);
+return 0;
+}
+
+  
