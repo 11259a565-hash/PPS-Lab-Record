4b7c8306dfb17e4321a990d08f462347f7c99a49
11259a565-hash <11259a565@kanchiuniv.ac.in>
2026-05-02 00:20:51 +0530

    Matrix Subtraction

---
+++ program_19.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int r, c, i, j;
+    int a[10][10], b[10][10], diff[10][10];
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &r, &c);
+    printf("Enter elements of Matrix A:\n");
+    for (i = 0; i < r; ++i) {
+        for (j = 0; j < c; ++j) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    printf("Enter elements of Matrix B:\n");
+    for (i = 0; i < r; ++i) {
+        for (j = 0; j < c; ++j) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+    for (i = 0; i < r; ++i) {
+        for (j = 0; j < c; ++j) {
+            diff[i][j] = a[i][j] - b[i][j];
+        }
+    }
+    printf("\nResultant Matrix (A - B):\n");
+    for (i = 0; i < r; ++i) {
+        for (j = 0; j < c; ++j) {
+            printf("%d  ", diff[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
